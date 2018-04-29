82a4c848eec8bd3fd4fa4ccb9a2dd4cb207c7250
shishir99111 <shishir99111@gmail.com>
2018-04-30 00:13:13 +0530

    utils

---
+++ utils/index.js
@@ -0,0 +1,54 @@
+const moment = require('moment');
+
+function getJoiErrors(error) {
+  if (error && error.isJoi) {
+    const errors = error.details.map((error) => {
+      return error.message;
+    });
+    return errors.join(',');
+  }
+  return '';
+};
+
+function getAppName() {
+  return require('../package.json').name;
+}
+
+function toUTCDate(date) {
+  return moment.utc(date).format('DD-MM-YYYYTHH:mm:ss');
+}
+
+function sanitizeObj(obj) {
+  Object.keys(obj).forEach((key) => {
+    const val = obj[key];
+    if (typeof val !== 'boolean') {
+      if (val) {
+        obj[key] = val || '';
+      } else {
+        delete obj[key];
+      }
+    }
+  });
+  return obj;
+}
+
+function trimObject(obj) {
+  let value;
+  Object.keys(obj).forEach((key) => {
+    value = obj[key];
+    if (value && typeof value === 'string') {
+      obj[key] = value.trim();
+    } else if (value && value.constructor === Object && typeof value === 'object') {
+      obj[key] = trimObject(value);
+    }
+  });
+  return obj;
+}
+
+module.exports = {
+  getJoiErrors,
+  getAppName,
+  toUTCDate,
+  sanitizeObj,
+  trimObject,
+};
