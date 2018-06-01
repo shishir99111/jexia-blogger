83be68491c80d7f4e9536622d5c9940004c624e4
shishir99111 <shishir99111@gmail.com>
2018-06-02 01:38:24 +0530

    default case

--- web/router/api/common/postRecord.handler.js
+++ web/router/api/common/postRecord.handler.js
@@ -1,7 +1,9 @@
 const Boom = require('boom');
-const { createDatasetInstance, checkIfValidDataset } = rootRequire('utils');
+const Joi = require('joi');
+
+const { createDatasetInstance, checkIfValidDataset, getErrorMessages } = rootRequire('utils');
 const { postUserSchema, postBlogSchema, postCommentSchema } = rootRequire('joi');
-const { DATASETS } = rootRequire('constants');
+// const { DATASETS } = rootRequire('constants');
 
 const schemaSelection = function(dataset) {
   switch (dataset) {
@@ -11,18 +13,24 @@ const schemaSelection = function(dataset) {
       return postBlogSchema;
     case 'user':
       return postUserSchema;
+    default:
+      return Boom.badRequest('Not a valid Dataset');
   }
-}
+};
 
 async function logic({ body, params }) {
-  if (!checkIfValidDataset(params.dataset)) Boom.badRequest('Invalid Dataset');
+  try {
+    if (!checkIfValidDataset(params.dataset)) Boom.badRequest('Invalid Dataset');
 
-  const { error } = Joi.validate(body, schemaSelection(params.dataset), { abortEarly: false });
-  if (error) throw Boom.badRequest(getErrorMessages(error));
+    const { error } = Joi.validate(body, schemaSelection(params.dataset), { abortEarly: false });
+    if (error) throw Boom.badRequest(getErrorMessages(error));
 
-  const dataset = await createDatasetInstance(params.dataset);
-  const _body = Array.isArray(body) ? body : [body];
-  return (await dataset.insert(_body).execute());
+    const dataset = await createDatasetInstance(params.dataset);
+    const _body = Array.isArray(body) ? body : [body];
+    return dataset.insert(_body).execute();
+  } catch (e) {
+    throw e;
+  }
 }
 
 function handler(req, res, next) {
